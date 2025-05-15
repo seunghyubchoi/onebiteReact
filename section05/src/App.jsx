edd091d36d3a144da4d600111d8509c5e6afa158
Seunghyub choi <seunghyub.choi@gmail.com>
2025-05-15 21:26:13 +0900

    state/props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,28 +1,13 @@
 import './App.css';
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import Bulb from "./components/Bulb"
+import Counter from "./components/Counter"
 
 function App() {
-
-  const buttonProps = {
-    text : "메일"
-    , color : "red"
-    , a: 1
-    , b: 2
-    , c: 3
-  }
-
+  
   return (
     <>
-      <Button {...buttonProps}/>
-      <Button text={"카페"}>
-        <Header/>
-      </Button>
-      <Button text={"블로그"}>
-        <div>자식 요소</div>
-      </Button>
+      <Bulb />
+      <Counter />
     </>
   )
 }
